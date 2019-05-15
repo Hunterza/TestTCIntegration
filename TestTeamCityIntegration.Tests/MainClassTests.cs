b314e55fd2448330be2af7c5affc003fab8d1c3e
Hunterza <kanapol@chiabro.com>
2019-05-15 10:18:08 +0700

    Add test project

---
+++ TestTeamCityIntegration.Tests/MainClassTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace TestTeamCityIntegration.Tests
+{
+    [TestFixture]
+    public class MainClassTests
+    {
+        private MainClass manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            manager = new MainClass();
+        }
+
+        [Test]
+        public void Sum_Tests()
+        {
+            // Assert
+            Assert.AreEqual(3, manager.Sum(1, 2));
+        }
+    }
+}
